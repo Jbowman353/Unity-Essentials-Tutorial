3e0d359f0bd79b1f76dd4b84844833a5758bb9f6
Josh Bowman <josh@Joshs-MacBook-Pro.local>
2025-09-22 15:17:47 -0400

    Initial commit

---
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public float rotationSpeed;
+    public GameObject onCollectEffect;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            Destroy(gameObject);
+            Instantiate(onCollectEffect, transform.position, transform.rotation);
+        }
+    }
+}
